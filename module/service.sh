33b7e3b4c8b3cb7fade14b1de855c10f9495781c
Rem01Gaming <Rem01_Gaming@proton.me>
2025-01-04 21:21:05 +0700

    module/service: Make sure iptables rules set properly
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- module/service.sh
+++ module/service.sh
@@ -1,10 +1,17 @@
 # Wait until boot completed
-until [ "$(getprop sys.boot_completed)" = "1" ] && [ -d /data/data ]; do
-    sleep 1
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+	sleep 1
 done
 
 packages="$(cat /data/adb/net-switch/isolated.json | tr -d '[]" ' | tr ',' ' ')"
 for apk in $packages; do
+	loop=1
+	until [ -d /data/data/$apk ]; do
+		((loop++))
+		[ $loop -eq 20 ] && break
+		sleep 3
+	done
+
 	uid="$(stat -c '%u' /data/data/$apk)"
 	iptables -I OUTPUT -m owner --uid-owner $uid -j REJECT
 	ip6tables -I OUTPUT -m owner --uid-owner $uid -j REJECT
